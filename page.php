bba63689abdd06c3bbf1c58a1503810f4623700c
andywarthol <andywarthol@gmail.com>
2015-04-13 15:37:28 -0700

    General content page

--- page.php
+++ page.php
@@ -5,12 +5,7 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="row">
-					<div class="col-sm-4 col-md-3">
-						<?php
-							get_sidebar();
-						?>
-					</div>
-					<div class="col-sm-8 col-md-9 col-lg-8">
+					<div class="col-sm-12 col-md-10-offset-1 col-lg-8 col-lg-offset-2">
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<div class="row">
 							<div class="col-sm-12">
