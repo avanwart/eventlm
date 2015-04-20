84488d03856bd032df50cdf0c48061d95da66076
andywarthol <andywarthol@gmail.com>
2015-04-20 16:08:42 -0700

    Phone number color, lightening hero image, events section

--- home-page.php
+++ home-page.php
@@ -27,7 +27,7 @@ Template Name: Home Page
 				<!-- CTA -->
 				<div class="row">
 					<div class="col-sm-12">
-						<button class="btn btn-lg btn-primary" id="toggle_intake">Request a Free Consultation <i class="glyphicon glyphicon-chevron-right"></i></button>
+						<button class="btn btn-lg btn-primary" id="toggle_intake">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></button>
 					</div>
 				</div>
 			</div>
@@ -86,7 +86,7 @@ Template Name: Home Page
 												<li>Budget Forecasting</li>
 											</ul>
 											<p class="text-center">
-												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a Free Consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
+												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
 												<a href="#" class="btn btn-lg btn-default">Learn more <i class="glyphicon glyphicon-chevron-right"></i></a>
 											</p>
 										</div>
@@ -110,7 +110,7 @@ Template Name: Home Page
 												<li>Crowd and safety management</li>
 											</ul>
 											<p class="text-center">
-												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a Free Consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
+												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
 												<a href="#" class="btn btn-lg btn-default">Learn more <i class="glyphicon glyphicon-chevron-right"></i></a>
 											</p>
 										</div>
@@ -129,7 +129,7 @@ Template Name: Home Page
 											<h2>Budget Reduction Services</h2>
 											<p>With a database of over 1200 vendors and partners, we can ensure that the right equipment and services are delivered to our client at the right price.  Through proven relationships and industry standing, Event Logistics Management has the ability to secure vehicles of all types at below market value.  These relationships significantly reduce operational costs for our clients and also for passengers.</p>
 											<p class="text-center">
-												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a Free Consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
+												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
 												<a href="#" class="btn btn-lg btn-default">Learn more <i class="glyphicon glyphicon-chevron-right"></i></a>
 											</p>
 										</div>
@@ -148,7 +148,7 @@ Template Name: Home Page
 											<h2>Parking Solutions</h2>
 											<p>As part of the event experience, the Event Logistics Management team will become familiar and comfortable with the venue layout, parking facilities and shuttle drop and load zone opportunities.  We will work alongside city and local agencies to optimize ingress and egress for the event, and produce a solution which reduces the impact of the event to its guests and the local community.</p>
 											<p class="text-center">
-												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a Free Consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
+												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
 												<a href="#" class="btn btn-lg btn-default">Learn more <i class="glyphicon glyphicon-chevron-right"></i></a>
 											</p>
 										</div>
@@ -173,7 +173,7 @@ Template Name: Home Page
 												<li>Sedan, SUV, Limousines and Vans</li>
 											</ul>
 											<p class="text-center">
-												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a Free Consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
+												<a class="btn btn-lg btn-primary" id="toggle_intake">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></a>
 												<a href="#" class="btn btn-lg btn-default">Learn more <i class="glyphicon glyphicon-chevron-right"></i></a>
 											</p>
 										</div>
@@ -214,56 +214,45 @@ Template Name: Home Page
 				<!-- CTA -->
 				<div class="row">
 					<div class="col-sm-12 text-center">
-						<button class="btn btn-lg btn-default" id="toggle_intake">Request a Free Consultation <i class="glyphicon glyphicon-chevron-right"></i></button>
+						<button class="btn btn-lg btn-default" id="toggle_intake">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></button>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-<section class="faq light">
+<section class="events">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1">
-				<h1>Frequently Asked Questions</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12 col-md-8 col-md-offset-2">
-				<?php if(get_field('faq')): ?>
- 
-					<div class="panel-group" id="accordion">
-
-					<?php while(has_sub_field('faq')): ?>
-
-						<div class="panel panel-default">
-							<div class="panel-heading">
-								<h4 class="panel-title">
-									<a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php the_sub_field('id'); ?>" class="collapsed">
-										<i class="glyphicon glyphicon-plus"></i><i class="glyphicon glyphicon-minus"></i>	<?php the_sub_field('question'); ?>
-									</a>
-								</h4>
-							</div>
-							<div id="collapse<?php the_sub_field('id'); ?>" class="panel-collapse collapse">
-								<div class="panel-body">
-									<?php the_sub_field('answer'); ?>
-								</div>
-							</div>
-						</div>
-				
-					<?php endwhile; ?>
-				
+			<div class="col-md-11 col-md-offset-1">
+				<div class="row">
+					<div class="col-sm-12">
+						<h1>Major Events Made Easy</h1>
 					</div>
-				
-				<?php endif; ?>
-				
+				</div>
+				<div class="row">
+					<div class="col-sm-7">
+						<h3>Types of events we regularly service:</h3>
+						<ul>
+							<li>Conventions</li>
+							<li>Sporting Events</li>
+							<li>Festivals / Concerts</li>
+							<li>Golf Tournaments</li>
+							<li>Corporate Events</li>
+							<li>National Political Conventions</li>
+							<li>Disaster Relief</li>
+							<li>Government Services</li>
+							<li>Marathons</li>
+							<li><a href="events">Learn more&rarr;</a></li>
+						</ul>
+						<div class="text-center"><a href="#" class="btn btn-lg btn-primary">Request a free consultation <i class="glyphicon glyphicon-chevron-right"></i></a></div>
+					</div>
+					<div class="col-sm-5">
+						<img src="<?php bloginfo('template_directory'); ?>/library/images/events-montage-2.jpg" alt="" class="img-responsive">
+					</div>
+				</div>
 			</div>
 		</div>
-		<!-- <div class="row">
-			<div class="col-sm-12">
-				<p class="text-center"><a href="#hero" class="btn btn-lg btn-default signup">Submit your RFP <i class="glyphicon glyphicon-chevron-right"></i></a></p>
-			</div>
-		</div> -->
 	</div>
 </section>
 <script>
@@ -349,6 +338,8 @@ Template Name: Home Page
 				window.location = "quick-quote-thank-you"
 			}
 		});
+
+		$('.carousel').carousel();
 
 	});
 </script>
