89d1642bd702f5fcf1af38f1c36e2ddf5b1cfcd5
andywarthol <andywarthol@gmail.com>
2015-05-04 19:08:20 -0700

    Adding sidebar to about page

--- about.php
+++ about.php
@@ -12,16 +12,18 @@ Template Name: About
 			<div class="col-sm-12">
 				<div class="row">
 					<div class="col-sm-4 col-md-3">
-						<?php get_sidebar(); ?>
+						<?php
+							get_sidebar();
+						?>
 					</div>
 					<div class="col-sm-8 col-md-9 col-lg-8">
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<div class="row">
 							<div class="col-sm-12">
-								<header>
-									<h1 class="page-title"><?php the_title(); ?></h1>
-								</header>
-								<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+								<article>
+									<header>
+										<h1 class="page-title"><?php the_title(); ?></h1>
+									</header>
 									<?php the_content(); ?>
 								</article> <!-- end article -->
 							</div>
