fa899df5c92f28373f5366b6923ac4a175f3f7de
andywarthol <andywarthol@gmail.com>
2015-05-04 19:02:50 -0700

    Contact page custom field

--- thank-you.php
+++ thank-you.php
@@ -6,21 +6,14 @@ Template Name: Thank You Template
 
 <?php get_header(); ?>	
 
-<section class="">
+<section class="thank-you">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12">
-				<?php if (function_exists("builder_breadcrumb_lists")) { ?>
-					<?php builder_breadcrumb_lists(); ?>
-				<?php } ?>
-			</div>
-		</div>
-		<div class="row">
 			<div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-				<h1 class="text-center"><?php the_field('thank_you_headline'); ?></h1>
+				<h1 class="text-center">Thank You</h1>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<?php the_content(); ?>				
-				<p class="text-center"><?php the_field('thank_you_messaging'); ?></p>
-				<p class="text-center"><a href="<?php the_field('thank_you_url'); ?>">Continue browsing &rarr;</a></p>
+				<?php endwhile; endif; ?>
 			</div>			
 		</div>
 	</div>
